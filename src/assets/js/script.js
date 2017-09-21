ec24a9ba4befc8da666b06ee68bb323efdca762e
DSIW <dsiw@dsiw-it.de>
2017-09-21 20:17:41 +0200

    Add missing ;

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,14 +1,14 @@
 Dropzone.autoDiscover = false;
 
-INIT_LON = 52.51663871100423
-INIT_LAT = 13.412246704101564
-INIT_ZOOM = 4
-HEAT_RADIUS = 5
-HEAT_BLUR = 10
+INIT_LON = 52.51663871100423;
+INIT_LAT = 13.412246704101564;
+INIT_ZOOM = 4;
+HEAT_RADIUS = 5;
+HEAT_BLUR = 10;
 
 state = function(message) {
   $('#status').text(message);
-}
+};
 
 var map = L.map('map', {
   center: [INIT_LON, INIT_LAT],
@@ -17,7 +17,7 @@ var map = L.map('map', {
   attributionControl: true,
   dragging: true
 });
-heat = L.heatLayer([], {radius: HEAT_RADIUS, blur: HEAT_BLUR}).addTo(map)
+heat = L.heatLayer([], {radius: HEAT_RADIUS, blur: HEAT_BLUR}).addTo(map);
 
 // map.on('click', function(ev) {
 //   $('#location').text(ev.latlng.lat + ", " + ev.latlng.lng)
@@ -28,16 +28,16 @@ addOSMTileLayer = function() {
   var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
   var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
   new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 19, attribution: osmAttrib}).addTo(map);
-}
+};
 
 addMapBoxTileLayer = function() {
   accessToken = 'pk.eyJ1IjoiZHNpdyIsImEiOiJjaXB2bmt0M2wwMDVxaHdrc3AwM2N4OHk0In0.kHVayjzVrUycpA2prqRhOg';
-  attribution = '© <a href="https://www.mapbox.com/feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-  url = 'https://api.mapbox.com/v4/mapbox.light/{z}/{x}/{y}.png'
+  attribution = '© <a href="https://www.mapbox.com/feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>';
+  url = 'https://api.mapbox.com/v4/mapbox.light/{z}/{x}/{y}.png';
   new L.TileLayer(url+'?access_token=' + accessToken, {
       attribution: attribution
   }).addTo(map);
-}
+};
 
 processFile = function(file) {
   var latlngs = [];
@@ -48,20 +48,20 @@ processFile = function(file) {
     latlngs.push([location.latitudeE7 * SCALAR_E7, location.longitudeE7 * SCALAR_E7]);
     return oboe.drop;
   }).done(function() {
-    state('Generating map...')
-    heat._latlngs = latlngs
+    state('Generating map...');
+    heat._latlngs = latlngs;
     heat.redraw();
 
-    $(".modal").modal("close")
+    $(".modal").modal("close");
     state("0%");
-    $(".progress .determinate").width('0%')
-    Materialize.toast(latlngs.length.toLocaleString() + " locations added", 4000)
+    $(".progress .determinate").width('0%');
+    Materialize.toast(latlngs.length.toLocaleString() + " locations added", 4000);
   });
 
   parseJSONFile(file, ob);
-}
+};
 
-parseJSONFile = function(file, ob) {
+parseJSONFile = function parseJSONFile(file, ob) {
   chunkReaderBlock = function(_offset, length, _file) {
     var r = new FileReader();
     var blob = _file.slice(_offset, length + _offset);
@@ -79,7 +79,7 @@ parseJSONFile = function(file, ob) {
       var chunk = evt.target.result;
       var percentLoaded = (100 * offset / fileSize).toFixed(0);
       state(percentLoaded + '% of ' + prettyFileSize + ' loaded...');
-      $(".progress .determinate").width(percentLoaded+'%')
+      $(".progress .determinate").width(percentLoaded+'%');
       ob.emit('data', chunk); // callback for handling read chunk
     } else {
       return;
@@ -95,7 +95,7 @@ parseJSONFile = function(file, ob) {
 
   // start parsing first chunk
   chunkReaderBlock(offset, chunkSize, file);
-}
+};
 
 /* https://github.com/davglass/prettysize */
 (function(){var sizes=["Bytes","kB","MB","GB","TB","PB","EB"];window.prettySize=function(e,s,i){var t,n;return sizes.forEach(function(n,o){i&&(n=n.slice(0,1));var r,B=Math.pow(1024,o);B>e||(r=(e/B).toFixed(1)+"",r.indexOf(".0")===r.length-2&&(r=r.slice(0,-2)),t=r+(s?"":" ")+n)}),t||(n=i?sizes[0].slice(0,1):sizes[0],t="0"+(s?"":" ")+n),t};}());
@@ -103,16 +103,16 @@ parseJSONFile = function(file, ob) {
 
 $(function () {
   // addOSMTileLayer()
-  addMapBoxTileLayer()
+  addMapBoxTileLayer();
 
   // init default config
   $('.modal').modal({
     dismissible: false
-  })
+  });
 
   setTimeout(function() {
-    $(".modal").modal('open')
-  }, 500)
+    $(".modal").modal('open');
+  }, 500);
 
   dropzone = new Dropzone("#dropzone", {
     url: '/',
@@ -121,7 +121,7 @@ $(function () {
     uploadMultiple: false,
     clickable: true,
     accept: function (file, done) {
-      processFile(file)
+      processFile(file);
     }
-  })
+  });
 });
